11666d4225cb572cb75f5fe136a4674a8c9e06c7
p-jahangiri <parviz.21.j@gmail.com>
2022-04-12 23:04:36 +0430

    delet defult stories

--- stories/components/card.stories.tsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import { ActionCard } from "../../components/ActionCard";
-
-// export default {
-//     title: "card/ActionCard",
-//     component: ActionCard,
-// }
-
-// export const CardActionArea = () => <ActionCard />;
